ce27c91568699bf5515f7a0ed381479725be4c89
Herbert Steininger <hsteininger@hsteininger.de>
2016-09-26 17:57:12 +0200

    set 'hosts' in script, also starts bash

--- start.sh
+++ start.sh
@@ -2,6 +2,13 @@ machinename=$(hostname);
 /etc/init.d/munge start
 slurmd -cv
 
+
+cat << EOF >> /etc/hosts
+172.18.0.2	cmaster
+172.18.0.3	cbackup
+172.18.0.4	cnode1
+EOF
+
 case ${machinename} in
 	cmast*)
 		slurmctld -cv
@@ -14,3 +21,5 @@ case ${machinename} in
 		#exit 1;
 		;;
 esac
+
+/bin/bash
